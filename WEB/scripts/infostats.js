f8f9b98b9eb934380b13592a3504094c309f73ef
S0MECoder <collin@alohalee.com>
2015-06-26 02:15:45 -0700

    Dev

---
+++ WEB/scripts/infostats.js
@@ -0,0 +1,19 @@
+/**** Version 1, 6/13/15 ****/
+function data() {
+    var stats = new XMLHttpRequest();
+    stats.open("GET", "file:///var/mobile/Library/Stats/BatteryStats.txt", true);
+    if (stats.readyState === 4 && (stats.status === 0 || stats.status === 200))
+    {
+        var text = stats.responseText;
+        var splitLine = text.split("/n");
+        var splitText = text.split(": ")[1];
+        var percent = splitLine[0].splitText;
+        var plugged = splitLine[1].splitText;
+        document.getElementById("battery").innerHTML = percent + "%" + " " + plugged;
+    }
+    stats.send();
+}
+setInterval(data, 1000);
+data();
+
+/**** Written by /u/S0MECoder. Modify the code all you want but please leave this comment intact. PM me on Reddit if you redistribute this code or a modified version so I can check out what you've made! ****/

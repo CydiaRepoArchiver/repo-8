54949a5e69a4674b96d57c7c0c3883181dc13107
S0MECoder <collin@alohalee.com>
2015-06-26 09:24:37 -0700

    test

--- WEB/scripts/infostats.js
+++ WEB/scripts/infostats.js
@@ -2,13 +2,15 @@
 function data() {
     var stats = new XMLHttpRequest();
     stats.open("GET", "scripts/BatteryStats.txt"/*"file:///var/mobile/Library/Stats/BatteryStats.txt"*/, true);
+    
     //if (stats.readyState === 4 && (stats.status === 0 || stats.status === 200))
     {
         var text = stats.responseText;
         var splitLine = text.split("/n")[0];
-        var percent = splitLine.split(": ")[1];
+        //var percent = splitLine.split(": ")[1];
         //var plugged = splitLine[1].splitText;
-        document.getElementById("battery").innerHTML = text+" ";//percent + "%";// + " " + plugged;
+        var percent = groovyAPI.getBatteryLevel() * 100;
+        document.getElementById("battery").innerHTML = percent + "%";// + " " + plugged;
     }
     stats.send();
 }

1c208e0d1f69cf509ca6033eb6d620de4baba3e9
S0MECoder <collin@alohalee.com>
2015-06-28 04:27:58 -0700

    site update and new upload

--- WEB/scripts/simpleweather.js
+++ WEB/scripts/simpleweather.js
@@ -12,6 +12,9 @@ function getWeather() {
     unit: tempUnits,
     success: function(weather) {
         document.getElementById("temp").innerHTML = weather.temp + "&deg"+ weather.units.temp +" "+ weather.currently;
+        var w = weather.currently;
+        var c = w.indexOf("loud") !== -1 ? "#a9a9a9" : w.indexOf("unn") !== -1 ? "#fada5e" : "#ccccff";
+        document.getElementById("temp").style.backgroundColor = c;
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');

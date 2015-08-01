f4d777f22d6e90cf950ad3db142793b5e4a8e7e7
S0MECoder <collin@alohalee.com>
2015-08-01 00:18:42 -0700

    test

--- Groovy/scripts/simpleweather.js
+++ Groovy/scripts/simpleweather.js
@@ -12,7 +12,7 @@ function getWeather() {
     location: cityName,
     unit: tempUnits,
     success: function(weather) {
-        html = '<table><tr> <td colspan = "2">' + weather.city + '</td> <td>' + weather.temp + '&deg' + weather.units.temp + '</td> </tr> <tr> <td colspan = "2">' + weather.currently + '</td> <td>' + /*groovyapi.getBatteryLevel +*/ '</td> </tr></table>';
+        html = '<table><tr> <td colspan = "2">' + weather.city + '</td> <td>' + weather.temp + '&deg' + weather.units.temp + '</td> </tr> <tr> <td colspan = "2">' + weather.currently + '</td> <td>' + groovyapi.getBatteryLevel + '</td> </tr></table>';
     },
     error: function(error) {
         html = '"<p>"+error+"</p>"';
